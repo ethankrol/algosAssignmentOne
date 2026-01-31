829deef4d0122e3d076442424209fbe1373179f1
Hugo Liu <98724522+hugoliu-code@users.noreply.github.com>
2026-01-30 21:11:12 -0500

    Add Print just in case

--- matching.py
+++ matching.py
@@ -179,7 +179,10 @@ def match(input_file_path = '', output_file_path = ''):
             # Otherwise, reject the pairing.
             else:
                 continue
-    
+
+    print("MATCHINGS:")
+    for i, matching in enumerate(first_matchings[1:]):
+        print(i+1, matching)
     write_output(output_file_path, first_matchings)
 
 if __name__ == "__main__":
